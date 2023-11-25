b0dcb949b628dc4c6d8374e8ef4c50a13bd64b29
sammyremi <adebayorsamuelas@gmail.com>
2023-11-25 17:52:42 +0100

    home page design

---
+++ app/javascript/controllers/react_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="react"
+export default class extends Controller {
+  connect() {
+
+    console.log("hello world");
+  }
+}
